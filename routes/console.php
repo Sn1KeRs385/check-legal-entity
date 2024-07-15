d25544a6e78b7f6b8cbf5188103f42bd9dcf7c9e
Dmitriy <dima.385@mail.ru>
2024-07-16 02:03:38 +0500

    Add timezone to scheduler

--- routes/console.php
+++ routes/console.php
@@ -5,4 +5,5 @@ declare(strict_types=1);
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::job(App\Jobs\UpdatePhysicalPersonOrganizationsJob::class)
+    ->timezone('Europe/Moscow')
     ->dailyAt('23:00');
