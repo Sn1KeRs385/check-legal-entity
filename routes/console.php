4429d302b5748e116b8f69a6e6b347d6dfabdc59
Dmitriy <dima.385@mail.ru>
2024-07-16 00:14:20 +0500

    Change schedule time

--- routes/console.php
+++ routes/console.php
@@ -5,4 +5,4 @@ declare(strict_types=1);
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::job(App\Jobs\UpdatePhysicalPersonOrganizationsJob::class)
-    ->dailyAt('18:00');
+    ->dailyAt('23:00');
